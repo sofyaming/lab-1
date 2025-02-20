2c24b0bb4a342c6d40c90e8037ea0045767b3e8e
Sofya Mingalieva <sofamingalieva@gmail.com>
2025-02-20 15:35:02 +0300

    feat: Add multiplication function to utils

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,13 @@ int Sum(int a, int b) {
 int Diff(int a, int b) {
 	return a - b;
 }
+int Mult(int a, int b) {
+	return a * b;
+}
 int main() {
 	int a = 2;
 	int b = 3;
 	std::cout << Sum(a, b) << "\n";
 	std::cout << Diff(a, b) << "\n";
+	std::cout << Mult(a, b) << "\n";
 }
